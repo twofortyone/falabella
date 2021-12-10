2f8ffea8d6e44333cc4272326b3b690e16b5a31a
Alejandro Parra <pa.parra12@uniandes.edu.co>
2021-12-10 17:14:45 -0500

    nc_santigo.py added

--- nc_santiago.py
+++ nc_santiago.py
@@ -1,12 +1,16 @@
+import config.nc_webhook as data_nc
+import pymsteams
+from datetime import datetime
+import pandas as pd 
 from os import rename, sep
 from textwrap import indent
-import pandas as pd 
 import numpy as np 
 import io
 from datetime import timedelta, date, datetime
 from os import listdir
 from os.path import isfile, join
 
+## -- Inicio files teams
 dt_string = datetime.now().strftime('%y%m%d')
 
 ## -------------------------------------------------- TODO incorporar método 
@@ -151,13 +155,68 @@ def guardar_res_tienda(df_mes, df_dia, df_hora, df_monto, df_nc_daily, tienda, d
        df_nc_daily.loc[df_nc_daily.Desc_local==tienda].to_excel(writer, sheet_name=f'NCs {date_str}', index=False)
        writer.save()
 
-tiendas = list(r2.Desc_local.unique())
+## -- Fin files teams
 
-for tienda in tiendas: 
+## -- Inicio webhook
+dwt = data_nc.dict_webhook_tiendas
+dst = data_nc.dict_sharepoint_tiendas
+dct = data_nc.ciudades_tiendas
+
+def send_msg(tienda, webhook, url_sp, ciudad, qty, sum, date):
+    myTeamsMessage = pymsteams.connectorcard(webhook, verify=False)
+    myTeamsMessage.title(f"Reporte de notas crédito | {date}")
+    myTeamsMessage.text(f'Tienda: {tienda} - {ciudad}')
+
+    section_1 = pymsteams.cardsection()
+    section_1.addFact("Cantidad:", qty)
+    section_1.addFact("Costo total:", sum)
+    myTeamsMessage.addSection(section_1)
+
+    myTeamsMessage.addLinkButton("Más información", url_sp)
+    myTeamsMessage.send()
+
+def send_msg_general(webhook, qty, sum, date):
+    myTeamsMessage = pymsteams.connectorcard(webhook, verify=False)
+    myTeamsMessage.title(f"Reporte de notas crédito | {date}")
+    myTeamsMessage.text('Resumen tiendas')
+    section_1 = pymsteams.cardsection()
+    section_1.addFact("Cantidad:", qty)
+    section_1.addFact("Costo total:", sum)
+    myTeamsMessage.addSection(section_1)
+    myTeamsMessage.send()
+
+
+df_nc = pd.read_csv('input/ncs_aut/ncs/output/nc_df.csv', dtype='object')
+df_nc.Mventa_nc = -pd.to_numeric(df_nc.Mventa_nc)
+
+df_nc.Dcompra_nvo = df_nc.Dcompra_nvo.str.replace('oct', '10')
+df_nc.Dcompra_nvo = df_nc.Dcompra_nvo.str.replace('nov', '11')
+df_nc.Dcompra_nvo = df_nc.Dcompra_nvo.str.replace('dic', '12')
+
+df_nc.Dcompra_nvo = pd.to_datetime(df_nc.Dcompra_nvo, format='%d-%m-%Y') # TODO arreglar para español 
+
+nc = df_nc.loc[df_nc['Tipo_trx']=='NC'] # Transaction type 
+local_excluir = ['3000', '2000', '11', '99', '321', '143']
+nc = nc.loc[~nc.Local_creacion.isin(local_excluir)] # Local number 
+#nc = nc.loc[nc['Cvendedor']!=47708] # Default seller 
+
+nc.loc[nc['Desc_local']=='MARTINA COLINA', 'Desc_local'] = 'COLINA'
+nc.loc[nc['Desc_local']=='MARTINA FONTANAR', 'Desc_local'] = 'FONTANAR'
+nc.loc[nc['Desc_local']=='EXPO HAYUELOS', 'Desc_local'] = 'HAYUELOS'
+
+df_ld = nc[nc.Dcompra_nvo == last_date]
+ld_str = last_date.strftime('%d-%b-%y')
+res = df_ld.groupby(['Desc_local']).agg({'Cautoriza':'nunique', 'Mventa_nc':'sum'})
+
+for tienda, row in res.iterrows():
+    monto = f'$ {row["Mventa_nc"]/1e6:,.1f} M'
+    cantidad = str(row['Cautoriza'])
+    send_msg(tienda, dwt[tienda], dst[tienda], dct[tienda], cantidad, monto, ld_str)
     guardar_res_tienda(r2, r5, r6, mayores_cienmil, nc_dia, tienda, last_date)
 
-print('Hecho')
-#nme2.to_excel(f'input/ncs_aut/output/{dt_string}_nc_base.xlsx', index=False)
-#r1.to_excel('input/ncs_aut/output/nc_test.xlsx', index=False)
-#r2.to_excel(f'input/ncs_aut/output/{dt_string}_nc_res.xlsx', index=False)
+qty = str(res.Cautoriza.sum())
+monto = f'$ {round(res.Mventa_nc.sum()/1e6)} M'
 
+send_msg_general(dwt['GENERAL'], qty, monto, ld_str)
+
+## -- Fin webhook
--- nc_webhook.py
+++ nc_webhook.py
@@ -3,28 +3,26 @@ import pymsteams
 from datetime import datetime
 import pandas as pd 
 
-dt_string = datetime.now().strftime('%d-%b-%y')
 dwt = data_nc.dict_webhook_tiendas
 dst = data_nc.dict_sharepoint_tiendas
 dct = data_nc.ciudades_tiendas
 
-def send_msg(tienda, webhook, url_sp, ciudad):
+def send_msg(tienda, webhook, url_sp, ciudad, qty, sum, date):
     myTeamsMessage = pymsteams.connectorcard(webhook, verify=False)
-    myTeamsMessage.title(f"Reporte de notas crédito | {dt_string}")
+    myTeamsMessage.title(f"Reporte de notas crédito | {date}")
     myTeamsMessage.text(f'Tienda: {tienda} - {ciudad}')
 
     section_1 = pymsteams.cardsection()
-    section_1.title("Resumen")
-    section_1.addFact("Cantidad:", "58")
-    section_1.addFact("Costo total:", "4 M")
+    section_1.addFact("Cantidad:", qty)
+    section_1.addFact("Costo total:", sum)
     myTeamsMessage.addSection(section_1)
 
     myTeamsMessage.addLinkButton("Más información", url_sp)
     myTeamsMessage.send()
 
-def send_msg_general(webhook, qty, sum):
+def send_msg_general(webhook, qty, sum, date):
     myTeamsMessage = pymsteams.connectorcard(webhook, verify=False)
-    myTeamsMessage.title(f"Reporte de notas crédito | {dt_string}")
+    myTeamsMessage.title(f"Reporte de notas crédito | {date}")
     myTeamsMessage.text('Resumen tiendas')
     section_1 = pymsteams.cardsection()
     section_1.addFact("Cantidad:", qty)
@@ -35,7 +33,12 @@ def send_msg_general(webhook, qty, sum):
 
 df_nc = pd.read_csv('input/ncs_aut/ncs/output/nc_df.csv', dtype='object')
 df_nc.Mventa_nc = -pd.to_numeric(df_nc.Mventa_nc)
-df_nc.Dcompra_nvo = pd.to_datetime(df_nc.Dcompra_nvo) # TODO arreglar para español 
+
+df_nc.Dcompra_nvo = df_nc.Dcompra_nvo.str.replace('oct', '10')
+df_nc.Dcompra_nvo = df_nc.Dcompra_nvo.str.replace('nov', '11')
+df_nc.Dcompra_nvo = df_nc.Dcompra_nvo.str.replace('dic', '12')
+
+df_nc.Dcompra_nvo = pd.to_datetime(df_nc.Dcompra_nvo, format='%d-%m-%Y') # TODO arreglar para español 
 
 nc = df_nc.loc[df_nc['Tipo_trx']=='NC'] # Transaction type 
 local_excluir = ['3000', '2000', '11', '99', '321', '143']
@@ -44,6 +47,7 @@ nc = nc.loc[~nc.Local_creacion.isin(local_excluir)] # Local number
 
 nc.loc[nc['Desc_local']=='MARTINA COLINA', 'Desc_local'] = 'COLINA'
 nc.loc[nc['Desc_local']=='MARTINA FONTANAR', 'Desc_local'] = 'FONTANAR'
+nc.loc[nc['Desc_local']=='EXPO HAYUELOS', 'Desc_local'] = 'HAYUELOS'
 
 last_date = nc.Dcompra_nvo.max().strftime('%d-%b-%y')
 df_ld = nc[nc.Dcompra_nvo == last_date]
@@ -52,9 +56,9 @@ res = df_ld.groupby(['Desc_local']).agg({'Cautoriza':'nunique', 'Mventa_nc':'sum
 for tienda, row in res.iterrows():
     monto = f'$ {row["Mventa_nc"]/1e6:,.1f} M'
     cantidad = str(row['Cautoriza'])
-    send_msg(tienda, dwt[tienda], dst[tienda], dct[tienda], cantidad,monto )
+    send_msg(tienda, dwt[tienda], dst[tienda], dct[tienda], cantidad, monto, last_date)
 
 qty = str(res.Cautoriza.sum())
 monto = f'$ {round(res.Mventa_nc.sum()/1e6)} M'
 
-send_msg_general(dwt['GENERAL'], qty, monto)
+send_msg_general(dwt['GENERAL'], qty, monto, last_date)
